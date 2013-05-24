3dc6a4584080db1b67f2c258d55750e62fbc8dbb
m.mathieu <m.mathieu@13795720-a1b2-11e2-9c9c-f342b7f990ca>
2013-05-24 12:04:33 +0000

    git-svn-id: https://svn.noc.fh-aachen.de/RechnernetzeSS2013/Praktikum/Workspace/MazeNet@65 13795720-a1b2-11e2-9c9c-f342b7f990ca

--- src/networking/MazeComMessageFactory.java
+++ src/networking/MazeComMessageFactory.java
@@ -25,7 +25,7 @@ public class MazeComMessageFactory {
 		mc.setMcType(MazeComType.ERROR);
 		mc.setId(playerID);
 		mc.setErrorMessage(of.createErrorMessageType());
-		mc.getErrorMessage().setError(et);
+		mc.getErrorMessage().setErrorCode(et);
 		return mc;
 	}
 
