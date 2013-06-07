3bece955a466481c77968d5e8706f5a4226937f5
bongen <bongen@13795720-a1b2-11e2-9c9c-f342b7f990ca>
2013-06-07 11:38:50 +0000

    UTF InputStream Patch
    git-svn-id: https://svn.noc.fh-aachen.de/RechnernetzeSS2013/Praktikum/Workspace/MazeNet@135 13795720-a1b2-11e2-9c9c-f342b7f990ca

--- src/networking/UTFInputStream.java
+++ src/networking/UTFInputStream.java
@@ -1,24 +1,20 @@
 package networking;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 
-
-public class UTFInputStream{
+public class UTFInputStream {
 
 	private InputStream is;
 
 	public UTFInputStream(InputStream stream) {
-		is = stream;
+		this.is = stream;
 	}
 
 	public String readUTF8() throws IOException {
 
-		while (is.available() < 4) {
-			Thread.yield();
-		}
-		byte[] tmp = new byte[4];
-		is.read(tmp);
+		byte[] tmp = this.readNBytes(4);
 
 		int len = 0;
 		len |= (tmp[3] & 0xff);
@@ -29,19 +25,35 @@ public class UTFInputStream{
 		len <<= 8;
 		len |= (tmp[0] & 0xff);
 
-		byte[] bytes = new byte[len];
-		while (is.available() < len) {
-			Thread.yield();
-		}
-		is.read(bytes);
+		byte[] bytes = this.readNBytes(len);
 		String message = new String(bytes, "UTF-8");
 		return message;
 	}
-	
+
 	public void close() throws IOException {
 		this.is.close();
 	}
-	
+
+	private byte[] readNBytes(int n) throws IOException {
+		if (n <= 0) {
+			throw new IllegalArgumentException();
+		}
+		byte buf[] = new byte[n];
+
+		int readcount = 0;
+		int lastreadcount;
+		while (readcount < n) {
+			lastreadcount = this.is.read(buf, readcount, n - readcount);
+			if (lastreadcount == -1) {
+				throw new EOFException(
+						String.format(
+								"Could only get %d of %d requested bytes",
+								readcount, n));
+			}
+			readcount += lastreadcount;
+
+		}
+
+		return buf;
+	}
 }
-
-
