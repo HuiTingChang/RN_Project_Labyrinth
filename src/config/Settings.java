c91987707a9d8cee413aba77b1cbabbd0848b2c7
harald <h.bongen@gmail.com>
2015-06-10 08:39:36 +0200

    Settings tweaked

--- src/config/Settings.java
+++ src/config/Settings.java
@@ -26,23 +26,23 @@ public class Settings {
 	 * Die Zeit in Milisekunden, nach der ein Logintimeout eintritt LOGINTIMEOUT
 	 * = 60000 entspricht einer Minute
 	 */
-	public final static long LOGINTIMEOUT = 60 * 60000;
+	public final static long LOGINTIMEOUT = 2 * 60000;
 	public final static int LOGINTRIES = 3;
 	/**
 	 * Die Zeit in Milisekunden, die die Animation eines Zug (die Bewegung des
 	 * Pins) benoetigen soll
 	 */
-	public final static int MOVEDELAY = 1000;
+	public final static int MOVEDELAY = 200;
 	/**
 	 * Die maximale Anzahl der Versuche einen gueltigen Zug zu uebermitteln
 	 */
 	public final static int MOVETRIES = 3;
 	public final static int PORT = 5123;
-	public final static long SENDTIMEOUT = 1 * 60 * 1000;
+	public final static long SENDTIMEOUT = 1 * 30 * 1000;
 	/**
 	 * Die Zeit in Milisekunden, die das Einschieben der Shiftcard dauern soll
 	 */
-	public final static int SHIFTDELAY = 1000;
+	public final static int SHIFTDELAY = 500;
 	/**
 	 * Wenn TESTBOARD = true ist, dann ist das Spielbrett bei jedem Start
 	 * identisch (zum Debugging)
