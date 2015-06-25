82c7b2dea7903c2bd8ca43ae85998fdd88734cfe
harald <h.bongen@gmail.com>
2015-06-25 14:30:44 +0200

    Merge conflicts solved

--- src/config/Settings.java
+++ src/config/Settings.java
@@ -25,24 +25,23 @@ public class Settings {
 	 * deshalb nicht final
 	 */
 	public static int DEFAULT_PLAYERS = 1;
-	public static String IMAGEFILEEXTENSION = ".png"; //$NON-NLS-1$
+	public  static String IMAGEFILEEXTENSION = ".png"; //$NON-NLS-1$
 	/**
 	 * Auf das angehaengte / achten
 	 */
-	public static String IMAGEPATH = "/server/userInterface/resources/"; //$NON-NLS-1$
-	public static Locale LOCALE = new Locale("de"); //$NON-NLS-1$
+	public  static String IMAGEPATH = "/server/userInterface/resources/"; //$NON-NLS-1$
+	public  static Locale LOCALE = new Locale("de"); //$NON-NLS-1$
 	/**
 	 * Die Zeit in Milisekunden, nach der ein Logintimeout eintritt LOGINTIMEOUT
 	 * = 60000 entspricht einer Minute
 	 */
-	public static long LOGINTIMEOUT = 2 * 60000;
-	public static int LOGINTRIES = 3;
+	public  static long LOGINTIMEOUT = 2 * 60000;
+	public  static int LOGINTRIES = 3;
 	/**
 	 * Die Zeit in Milisekunden, die die Animation eines Zug (die Bewegung des
 	 * Pins) benoetigen soll
 	 */
 	public static int MOVEDELAY = 300;
-
 	/**
 	 * Die maximale Anzahl der Versuche einen gueltigen Zug zu uebermitteln
 	 */
@@ -69,44 +68,41 @@ public class Settings {
 	public static UI USERINTERFACE = new BetterUI();
 
 	@SuppressWarnings("nls")
-	public static void reload(String path) {
-		Properties properties = new Properties();
+	public static void reload(String path){
+		Properties prop = new Properties();
 		try {
-			InputStream propStream = Settings.class.getResourceAsStream(path);
-			properties.load(propStream);
+			InputStream propStream=Settings.class.getResourceAsStream(path);
+			prop.load(propStream);;
 			propStream.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		MAX_NAME_LENGTH = Integer.parseInt(properties.get("MAX_NAME_LENGTH")
-				.toString());
-		DEFAULT_PLAYERS = Integer.parseInt(properties.get("DEFAULT_PLAYERS")
-				.toString());
-		IMAGEFILEEXTENSION = properties.getProperty("IMAGEFILEEXTENSION").toString();
-		IMAGEPATH = properties.getProperty("IMAGEPATH").toString();
-		LOCALE = new Locale(properties.get("LOCALE").toString());
-		LOGINTIMEOUT = Integer.parseInt(properties.get("LOGINTIMEOUT").toString());
-		LOGINTRIES = Integer.parseInt(properties.get("LOGINTRIES").toString());
-		MOVEDELAY = Integer.parseInt(properties.get("MOVEDELAY").toString());
-		MOVETRIES = Integer.parseInt(properties.get("MOVETRIES").toString());
-		PORT = Integer.parseInt(properties.get("PORT").toString());
-		SENDTIMEOUT = Integer.parseInt(properties.get("SENDTIMEOUT").toString());//.split(" * ")[0])*Integer.parseInt(prop.get("SENDTIMEOUT").toString().split(" * ")[1])*Integer.parseInt(prop.get("SENDTIMEOUT").toString().split(" * ")[2]); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
-		SHIFTDELAY = Integer.parseInt(properties.get("SHIFTDELAY").toString());
-		TESTBOARD = Boolean.parseBoolean(properties.get("TESTBOARD").toString()); //$NON-NLS-1$
-		TESTBOARD_SEED = Integer
-				.parseInt(properties.get("TESTBOARD_SEED").toString());
-		USERINTERFACE = properties.get("USERINTERFACE").toString().equals("BetterUI") ? new BetterUI() : null; //$NON-NLS-1$ //$NON-NLS-2$
-		// print();
+		MAX_NAME_LENGTH = Integer.parseInt(prop.get("MAX_NAME_LENGTH").toString());
+		DEFAULT_PLAYERS = Integer.parseInt(prop.get("DEFAULT_PLAYERS").toString()); 
+		IMAGEFILEEXTENSION = prop.getProperty("IMAGEFILEEXTENSION").toString(); 
+		IMAGEPATH = prop.getProperty("IMAGEPATH").toString(); 
+		LOCALE = new Locale(prop.get("LOCALE").toString()); 
+		LOGINTIMEOUT = Integer.parseInt(prop.get("LOGINTIMEOUT").toString());
+		LOGINTRIES = Integer.parseInt(prop.get("LOGINTRIES").toString()); 
+		MOVEDELAY = Integer.parseInt(prop.get("MOVEDELAY").toString()); 
+		MOVETRIES = Integer.parseInt(prop.get("MOVETRIES").toString()); 
+		PORT = Integer.parseInt(prop.get("PORT").toString()); 
+		SENDTIMEOUT = Integer.parseInt(prop.get("SENDTIMEOUT").toString());//.split(" * ")[0])*Integer.parseInt(prop.get("SENDTIMEOUT").toString().split(" * ")[1])*Integer.parseInt(prop.get("SENDTIMEOUT").toString().split(" * ")[2]); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
+		SHIFTDELAY = Integer.parseInt(prop.get("SHIFTDELAY").toString()); 
+		TESTBOARD = Boolean.parseBoolean(prop.get("TESTBOARD").toString()); //$NON-NLS-1$
+		TESTBOARD_SEED = Integer.parseInt(prop.get("TESTBOARD_SEED").toString()); 
+		USERINTERFACE = prop.get("USERINTERFACE").toString().equals("BetterUI")?new BetterUI():null; //$NON-NLS-1$ //$NON-NLS-2$
+		//print();
 	}
-
+	
 	private Settings() {
 	}
-
+	
 	@SuppressWarnings("nls")
-	public static void print() {
-		// TODO vervollstaendigen
-		System.out.println("Imagepath: " + Settings.IMAGEPATH);
-		System.out.println("Imageext: " + Settings.IMAGEFILEEXTENSION);
+	public static void print(){
+		//TODO vervollstaendigen
+		System.out.println("Imagepath: "+Settings.IMAGEPATH);
+		System.out.println("Imageext: "+Settings.IMAGEFILEEXTENSION);
 
 	}
 }
