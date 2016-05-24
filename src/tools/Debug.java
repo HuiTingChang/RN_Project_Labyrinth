30742703b8d08c4b65b6fb6d765d777142d9a6db
Marco Jagodzinska <jagodzinska@fh-aachen.de>
2016-05-24 10:45:58 +0200

    threadsafe

--- src/tools/Debug.java
+++ src/tools/Debug.java
@@ -2,26 +2,31 @@ package tools;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Debug {
 
-	static HashMap<OutputStream, DebugLevel> liste = new HashMap<OutputStream, DebugLevel>();
+	static HashMap<OutputStream, DebugLevel> liste = (HashMap<OutputStream, DebugLevel>) Collections
+			.synchronizedMap(new HashMap<OutputStream, DebugLevel>());
 
 	public static void addDebugger(OutputStream stream, DebugLevel level) {
 		liste.put(stream, level);
 	}
 
-	//TODO erlaeuterndes Javadoc
+	// TODO erlaeuterndes Javadoc
 	public static void print(String str, DebugLevel level) {
+		// FIXME string chaining, uasses
 		str += "\n"; //$NON-NLS-1$
-		for (OutputStream out : liste.keySet()) {
-			DebugLevel streamLevel = liste.get(out);
-			try {
-				if (streamLevel.value() >= level.value())
-					out.write(str.getBytes());
-			} catch (IOException e) {
-				e.printStackTrace();
+		synchronized (liste) {
+			for (OutputStream out : liste.keySet()) {
+				DebugLevel streamLevel = liste.get(out);
+				try {
+					if (streamLevel.value() >= level.value())
+						out.write(str.getBytes());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 
